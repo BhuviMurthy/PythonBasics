757e045e2b63cce66444dadccaabdf0a18c65f15
BhuviMurthy <121146581+BhuviMurthy@users.noreply.github.com>
2024-01-14 19:12:00 -0600

    Create 3_OddOrEven.py

---
+++ 3_OddOrEven.py
@@ -0,0 +1,9 @@
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+resultSet = number % 2
+# Write your code below this line 👇
+if resultSet == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
