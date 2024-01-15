0874065d85912b8d6a200c10cbbde243041ee142
BhuviMurthy <121146581+BhuviMurthy@users.noreply.github.com>
2024-01-14 19:25:38 -0600

    Update 3_RollerCoasterProgram.py

--- 3_RollerCoasterProgram.py
+++ 3_RollerCoasterProgram.py
@@ -5,8 +5,10 @@ height = int(input("What is your height in cms"))
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age?\n"))
-    if age >= 18:
-        print("The ticket cost is $21")
+    if age < 12:
+        print("The ticket cost is $5")
+    elif age <= 18:
+        print("The ticket cost is $7")
     else: 
         print("The ticket cost is $7")
 else:
