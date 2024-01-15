3e42cfae97d76d0148c62c74f2d0a3d51df97e6d
BhuviMurthy <121146581+BhuviMurthy@users.noreply.github.com>
2024-01-14 19:22:53 -0600

    Create 3_RollerCoasterProgram.py

---
+++ 3_RollerCoasterProgram.py
@@ -0,0 +1,13 @@
+#This is an example of Nested if else statement 
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cms"))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age?\n"))
+    if age >= 18:
+        print("The ticket cost is $21")
+    else: 
+        print("The ticket cost is $7")
+else:
+    print("You cannot ride the rollercoaster")
