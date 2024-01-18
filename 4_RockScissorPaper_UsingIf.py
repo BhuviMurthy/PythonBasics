afcaeec838129e9f01c4dd919c064a79670ee603
Bhuvaneshwari Murthy <121146581+BhuviMurthy@users.noreply.github.com>
2024-01-17 23:47:08 -0600

    Update 4_RockScissorPaper_UsingIf.py

--- 4_RockScissorPaper_UsingIf.py
+++ 4_RockScissorPaper_UsingIf.py
@@ -27,40 +27,40 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
-user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
-
-if user_input == "0":
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+print(f"User chose:{user_input}")
+if user_input == 0:
   print(rock)
-elif user_input == "1":
+elif user_input == 1:
   print(paper)
 else:
   print(scissors)
 
 random_number = random.randint(0,2)
 
-print("Computer chose:")
-if random_number == "0":
+print(f"Computer chose:{random_number}")
+if random_number == 0:
   print(rock)
-elif random_number == "1":
+elif random_number == 1:
   print(paper)
 else:
   print(scissors)
 
-if user_input == "0" and random_number == "0":
+if user_input == 0 and random_number == 0:
   print("Draw")
-elif user_input == "0" and random_number == "1":
+if user_input == 0 and random_number == 1:
   print("Computer won")
-elif user_input == "0" and random_number == "2":
+elif user_input == 0 and random_number == 2:
   print("You won")
-elif user_input == "1" and random_number == "0":
+elif user_input == 1 and random_number == 0:
   print("You Won")
-elif user_input == "1" and random_number == "1":
+elif user_input == 1 and random_number == 1:
   print("Draw")
-elif user_input == "1" and random_number == "2":
+elif user_input == 1 and random_number == 2:
   print("Computer won")
-elif user_input == "2" and random_number == "0":
+elif user_input == 2 and random_number == 0:
   print("Computer won")
-elif user_input == "2" and random_number == "1":
+elif user_input == 2 and random_number == 1:
   print("You won")
 else:
   print("Draw")
